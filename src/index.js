07a49fddafd90a3ea1ffad7171a0bbd5eb824aef
CPPtrueCoder <andierodin@gmail.com>
2020-03-19 04:21:52 +0300

    Finish counter with redux;

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-
 import {createStore} from "redux";
-
+const inc = document.getElementById('inc'),
+	dec = document.getElementById('dec'),
+	counter=document.getElementById('counter');
 const reducer = (state = 0, action) => {
 	
 	switch (action.type) {
 		case 'INC':
 			return state + 1;
+		case 'DEC':
+			return state - 1;
 		default:
 			return state;
 	}
 };
 
-const store = createStore(reducer);
-store.subscribe(()=>{
-	console.log(store.getState());
+const store = createStore (reducer);
+inc.addEventListener('click',()=>{
+	store.dispatch({type:'INC'});
+});
+dec.addEventListener('click',()=>{
+	store.dispatch({type:'DEC'});
 });
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+const update =()=>{
+counter.innerHTML= store.getState();
+};
+store.subscribe(update);
